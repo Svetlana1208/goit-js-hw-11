50e61f359a39051000d4a955166bfeeaea10da94
Svitlana Samoliuk <101995368+Svetlana1208@users.noreply.github.com>
2023-02-07 17:04:11 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from 'axios';
+import { fetchRequest } from "./js/fetch";
 
 
-const perPage = 40;
-let url;
 let form;
 let searchQuery;
 let markup;
-let page = 1;
 let totalPages;
 let counter = 0;
 
@@ -47,24 +44,6 @@ function onSearch(e) {
         })
 
     counter += 1;
-}
-
-async function fetchRequest() {
-    const key = '33264104-6177d05b85a0a5034084eaf54';
-    url = `https://pixabay.com/api/?key=${key}&q=${searchQuery}&fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads`;
-
-    try {
-        const response = await axios.get(`${url}`, {params: {
-            page: page,
-            per_page: perPage,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-        }});
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
 }
 
 function onMarkUp(result) {
---
+++ src/js/fetch.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { searchQuery} from "../index";
+
+
+let url;
+const perPage = 40;
+let page = 1;
+
+
+
+export async function fetchRequest() {
+    const key = '33264104-6177d05b85a0a5034084eaf54';
+    url = `https://pixabay.com/api/?key=${key}&q=${searchQuery}&fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads`;
+
+    try {
+        const response = await axios.get(`${url}`, {params: {
+            page: page,
+            per_page: perPage,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+        }});
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+}
